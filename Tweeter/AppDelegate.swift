23be21756b780c74658b333e63d6e7127bd0cabd
whuang8 <huang554@purdue.edu>
2017-02-20 01:19:24 -0500

    Tweeter setup

--- Tweeter/AppDelegate.swift
+++ Tweeter/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BDBOAuth1Manager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -41,6 +42,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        print(url.description)
+        let requestToken = BDBOAuth1Credential(queryString: url.query)
+        let twitterClient = BDBOAuth1SessionManager(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "", consumerSecret: "")
+        twitterClient?.fetchAccessToken(withPath: "/oauth/access_token", method: "POST", requestToken: requestToken, success: { (accessToken) in
+            print("request token: \(requestToken?.token)")
+            print("I got the access token \(accessToken?.token)")
+            
+            twitterClient?.get("/1.1/account/verify_credentials.json", parameters: nil, progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
+                print("account: \(response)")
+            }, failure: { (task: URLSessionDataTask?, error: Error) in
+                print("error: \(error.localizedDescription)")
+            })
+        }, failure: { (error) in
+            print("error: \(error?.localizedDescription)")
+        })
+        return true
+    }
 
 }
 
---
+++ Tweeter/LoginViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewController.swift
+//  Tweeter
+//
+//  Created by William Huang on 2/20/17.
+//  Copyright © 2017 William Huang. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        let twitterClient = BDBOAuth1SessionManager(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "bRFFweljdqXmeTyuCm5OeoTMm", consumerSecret: "alOB7bWzbseTWuChr6IN5qPoBaKvDdoWEBqnIPZHoFZBOK1mCW")
+        twitterClient?.deauthorize()
+        twitterClient?.fetchRequestToken(withPath: "/oauth/request_token", method: "GET", callbackURL: URL(string: "tweeter://oauth"), scope: nil, success: { (requestToken) in
+            print("I got a token \(requestToken?.token)")
+            let url = URL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken!.token!)")!
+            UIApplication.shared.open(url)
+            
+        }, failure: { (error) in
+            print("error \(error?.localizedDescription)")
+        })
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
