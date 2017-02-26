4619c4b8f80ee7f441543d6ba918d4881899edb0
whuang8 <huang554@purdue.edu>
2017-02-26 16:06:39 -0500

    Check for action counts

--- Tweeter/TweetCell.swift
+++ Tweeter/TweetCell.swift
@@ -11,11 +11,27 @@ import UIKit
 class TweetCell: UITableViewCell {
 
     @IBOutlet weak var profilePictureImageView: UIImageView!
+    @IBOutlet weak var replyCount: UILabel!
+    @IBOutlet weak var retweetCount: UILabel!
+    @IBOutlet weak var favoriteCount: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         profilePictureImageView.layer.cornerRadius = 5
         profilePictureImageView.clipsToBounds = true
+        checkCounts()
+    }
+    
+    func checkCounts() {
+        if replyCount.text == "0" {
+            replyCount.isHidden = true
+        }
+        if retweetCount.text == "0" {
+            retweetCount.isHidden = true
+        }
+        if favoriteCount.text == "0" {
+            favoriteCount.isHidden = true
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
